1013973afd01f8e0840b547b47c06f84286633b8
Faran Muhammad <92796969+muhafara@users.noreply.github.com>
2023-03-06 22:27:10 +0000

    Python codes

---
+++ Basic-Python-Programs/Basic-Fraction.py
@@ -0,0 +1,10 @@
+'''Write a code to workout 7/10 of 100 g.'''
+
+noOfGrams = 100
+partsToFind = 7
+eachPart = 10
+
+totalGrams = (partsToFind / eachPart) * 100
+
+print(f'{partsToFind} / {eachPart} of {noOfGrams} gm = {totalGrams}')
+
